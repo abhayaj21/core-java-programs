41f73fe2fdae1d02f77bf6ecd11e4299719a4708
abhayaj21 <abhayaj2121@gmail.com>
2026-05-01 07:43:50 +0530

    Pattern 21 program Complete

---
+++ for loop/Pattern21.java
@@ -0,0 +1,19 @@
+class Pattern21
+{
+	public static void main(String[] args) {
+		for(int i = 4; i >= 1; i--)
+		{
+			for(int j = 1; j <= 4; j++)
+			{
+				if(i == j)
+				{
+					System.out.print("1 ");
+				}
+				else {
+					System.out.print("0 ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
