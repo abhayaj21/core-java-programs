980fbe82fbfdbb7a3de70b72f6b4f006dbcbcece
abhayaj21 <abhayaj2121@gmail.com>
2026-05-02 07:27:41 +0530

    Pattern 28 Program Complete

---
+++ for loop/Pattern28.java
@@ -0,0 +1,19 @@
+class Pattern28
+{
+	public static void main(String[] args) {
+		for(int i = 5; i >= 1; i--)
+		{
+			for(int j = 1; j <= i; j++)
+			{
+				if(i % 2 == 0)
+				{
+					System.out.print("0 ");
+				}
+				else {
+					System.out.print("1 ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
