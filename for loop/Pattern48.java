92b8a94d629281472fb5a14f13f74c3a73371867
abhayaj21 <abhayaj2121@gmail.com>
2026-05-03 07:47:50 +0530

    Pattern 48 Program Complete

---
+++ for loop/Pattern48.java
@@ -0,0 +1,21 @@
+class Pattern48
+{
+	public static void main(String[] args) {
+		for(int i = 1; i <= 5; i++)
+		{
+			for(int j = 1; j <= i; j++)
+			{
+				System.out.print("  ");
+			}
+			for(int j = i; j <= 5; j++)
+			{
+				System.out.print(j + " ");
+			}
+			for(int j = 4; j >= i; j--)
+			{
+				System.out.print(j + " ");
+			}
+			System.out.println();;
+		}
+	}
+}
