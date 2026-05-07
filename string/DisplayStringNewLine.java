a0246304f9a42d3da1eb40d9c38c894f21664664
abhayaj21 <abhayaj2121@gmail.com>
2026-05-07 08:29:36 +0530

    Dispaly each word of string in new line

---
+++ string/DisplayStringNewLine.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class DisplayStringNewLine
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Any String: ");
+		String str = sc.nextLine();
+
+		String[] str2 = str.split(" ");
+
+		for(int i = 0; i < str2.length; i++)
+		{
+			System.out.println(str2[i]);
+		}
+	}
+}
