a491a1761a9d8f4069c3695560d467a75bf9d156
abhayaj21 <abhayaj2121@gmail.com>
2026-05-09 08:14:02 +0530

    Hierarchical inherictace pratice program

---
+++ oops/inheritance/hierarchical inheritance/HierarchicalInheritance1.java
@@ -0,0 +1,99 @@
+class Base
+{
+	protected int a;
+	protected int b;
+
+	public Base()
+	{
+		System.out.println("Base Class Constructor");
+	}
+
+	public void bSetVal(int a, int b)
+	{
+		this.a = a;
+		this.b = b;
+	}
+
+	public void bShow()
+	{
+		System.out.println("A = " + a);
+		System.out.println("B = " + b);
+	}
+
+	public int getA(){return a;}
+	public int getB(){return b;}
+}
+
+class Derrived extends Base
+{
+	protected int c;
+	protected int d;
+
+	public void dSetVal(int c, int d)
+	{
+		this.c = c;
+		this.d = d;
+	}
+
+	public Derrived()
+	{
+		System.out.println("Derrived Constructor");
+	}
+
+	public void dShow()
+	{
+		bShow();
+		System.out.println("C = " + c);
+		System.out.println("D = " + d);
+	}
+
+	public int getC(){return c;}
+	public int getD(){return d;}
+}
+
+
+class Sub extends Base
+{
+	protected int e;
+	protected int f;
+
+	public void sSetVal(int e, int f)
+	{
+		this.e = e;
+		this.f = f;
+	}
+
+	public Sub()
+	{
+		System.out.println("Sub Constructor");
+	}
+
+	public void sShow()
+	{
+		bShow();
+		System.out.println("E = " + e);
+		System.out.println("F = " + f);
+	}
+
+	public int getE(){return e;}
+	public int getF(){return f;}
+}
+
+class HierarchicalInheritance1
+{
+	public static void main(String[] args) {
+
+		/*Base b = new Base();
+		b.bShow();*/
+		Derrived d = new Derrived();
+		d.bSetVal(12,21);
+		d.dSetVal(32,12);
+		d.dShow();
+
+		Sub s = new Sub();
+		s.bSetVal(32,89);
+		s.sSetVal(43,21);
+		s.sShow();
+
+	}
+}
